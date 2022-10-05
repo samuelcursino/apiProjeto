9dc4ec564e29fbfe2f4b4be1506833982a326732
Samuel Cursino <samuelcursino11@gmail.com>
2022-10-04 21:21:28 -0300

    Fazendo as rotas funcionarem
    atualização no indes.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const rotasCategoria = require('./route/route');
+const rotasClientes = require('./route/route');
 const app = express();
+app.use('/', rotasClientes);
 app.listen(3000, ()=>{
     console.log('Servidor ta on na porta 3000 - http://localhost:3000');
 });
